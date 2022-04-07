07ff2c489f5952db3f805f94d4faf8e215257c3b
Kriszti <kecskeskriszti22@gmail.com>
2022-04-07 17:05:03 +0200

    react practice

---
+++ src/app2.js
@@ -0,0 +1,16 @@
+import './app2.css';
+
+function footer() {
+    return (
+        <>
+            <div></div>
+            <div className="footer">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+        </>
+    )
+};
+
+export default footer;
